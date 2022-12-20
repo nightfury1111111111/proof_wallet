153f299578cd3edb3b595d5138bddc4c85ec1ee4
nightfury1111111111 <linjay0208@gmail.com>
2022-12-19 21:23:19 -0800

    add remove wallet page

--- packages/extension/src/pages/setting/connections/basic-access.tsx
+++ packages/extension/src/pages/setting/connections/basic-access.tsx
@@ -1,41 +1,41 @@
-import React, { FunctionComponent, useMemo, useState } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { HeaderLayout } from "../../../layouts";
 
-import style from "../style.module.scss";
 import { useHistory } from "react-router";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../../stores";
-import { PageButton } from "../page-button";
-import {
-  ButtonDropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-} from "reactstrap";
+// import { PageButton } from "../page-button";
+// import {
+//   ButtonDropdown,
+//   DropdownItem,
+//   DropdownMenu,
+//   DropdownToggle,
+// } from "reactstrap";
 
 import styleConnections from "./style.module.scss";
 import { useIntl } from "react-intl";
 import { useConfirm } from "../../../components/confirm";
+import { Button } from "reactstrap";
 
 export const SettingConnectionsPage: FunctionComponent = observer(() => {
   const history = useHistory();
   const intl = useIntl();
 
   const { chainStore, permissionStore } = useStore();
-  const [selectedChainId, setSelectedChainId] = useState(
-    chainStore.current.chainId
-  );
+  const [selectedChainId] = useState(chainStore.current.chainId);
   const basicAccessInfo = permissionStore.getBasicAccessInfo(selectedChainId);
 
-  const [dropdownOpen, setOpen] = useState(false);
-  const toggle = () => setOpen(!dropdownOpen);
+  const [loading, setLoading] = useState(false);
+
+  // const [dropdownOpen, setOpen] = useState(false);
+  // const toggle = () => setOpen(!dropdownOpen);
 
   const confirm = useConfirm();
 
-  const xIcon = useMemo(
-    () => [<i key="remove" className="fas fa-times" />],
-    []
-  );
+  // const xIcon = useMemo(
+  //   () => [<i key="remove" className="fas fa-times" />],
+  //   []
+  // );
 
   return (
     <HeaderLayout
@@ -48,8 +48,8 @@ export const SettingConnectionsPage: FunctionComponent = observer(() => {
         history.goBack();
       }}
     >
-      <div className={style.container}>
-        <ButtonDropdown
+      <div style={{ marginTop: "15px" }}>
+        {/* <ButtonDropdown
           isOpen={dropdownOpen}
           toggle={toggle}
           className={styleConnections.dropdown}
@@ -73,12 +73,12 @@ export const SettingConnectionsPage: FunctionComponent = observer(() => {
               );
             })}
           </DropdownMenu>
-        </ButtonDropdown>
+        </ButtonDropdown> */}
         {basicAccessInfo.origins.map((origin) => {
           return (
-            <PageButton
-              title={origin}
+            <div
               key={origin}
+              className={styleConnections.connectBox}
               onClick={async (e) => {
                 e.preventDefault();
 
@@ -87,8 +87,8 @@ export const SettingConnectionsPage: FunctionComponent = observer(() => {
                     img: (
                       <img
                         alt="unlink"
-                        src={require("../../../public/assets/img/broken-link.svg")}
-                        style={{ height: "80px" }}
+                        src={`https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${origin}&size=64`}
+                        style={{ height: "64px" }}
                       />
                     ),
                     title: intl.formatMessage({
@@ -103,10 +103,51 @@ export const SettingConnectionsPage: FunctionComponent = observer(() => {
                   await basicAccessInfo.removeOrigin(origin);
                 }
               }}
-              icons={xIcon}
-            />
+            >
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <img
+                  style={{
+                    width: "46px",
+                    height: "46px",
+                    borderRadius: "50px",
+                  }}
+                  src={`https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${origin}&size=64`}
+                />
+                <div style={{ marginLeft: "11.36px" }}>
+                  <div className={styleConnections.urlname}>
+                    {origin.split("//")[1]}
+                  </div>
+                </div>
+              </div>
+              <img
+                style={{
+                  width: "36px",
+                  height: "36px",
+                  borderRadius: "50px",
+                }}
+                src={require("../../../public/assets/img/remove.svg")}
+              />
+            </div>
           );
         })}
+        <div style={{ height: "70px", color: "transparent" }} />
+        <div className={styleConnections.footer}>
+          <Button
+            data-loading={loading}
+            className={styleConnections.button}
+            onClick={() => {
+              setLoading(true);
+              Promise.all(
+                basicAccessInfo.origins.map(async (origin) => {
+                  await basicAccessInfo.removeOrigin(origin);
+                })
+              );
+              setLoading(false);
+            }}
+          >
+            Remove all
+          </Button>
+        </div>
       </div>
     </HeaderLayout>
   );
